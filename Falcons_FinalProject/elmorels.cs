73b9456ed9ebda2b211f6e67a86753427adbb589
Lou <Lou@DESKTOP-U6LN33K>
2024-11-30 17:53:27 -0500

    Add project files.

---
+++ Falcons_FinalProject/elmorels.cs
@@ -0,0 +1,21 @@
+﻿/*
+# Name:Luke Elmore
+# email:elmorels@mail.uc.edu
+# Assignment Title: Final Project
+# Due Date:12/10/2024
+# Course: IS 3050
+# Semester/Year:fall 2024
+# Brief Description: 
+# Citations:
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Falcons_FinalProject
+{
+    public class elmorels
+    {
+    }
+}
